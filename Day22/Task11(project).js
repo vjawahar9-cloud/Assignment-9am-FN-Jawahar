32284dead1db72ebf1a4304aa43999b995f8166b
vjawahar9-cloud <vjawahar9@gmail.com>
2025-12-16 09:33:29 +0530

    hii

---
+++ Day22/Task11(project).js
@@ -0,0 +1,24 @@
+Students can now:
+
+Create reusable helper functions in any project.
+
+Replace repeated code in loops or conditionals.
+
+Use functions to organize HTML button actions.
+
+Start creating modular scripts (e.g., calculators, forms).
+
+Use arrow functions for modern JS style (React-ready).
+
+Integrate into previous projects:
+
+Add greet() function to welcome users.
+
+Add calculator functions for math tasks.
+
+Add printing functions for loops (like printing arrays).
+
+
+
+
+
