552ce78a93a0a355c5409d7345a4dd8d43e7d4a1
vjawahar9-cloud <vjawahar9@gmail.com>
2025-12-30 09:21:57 +0530

    lkjkujhg

---
+++ Day26/Task 1 to 5.js
@@ -0,0 +1,13 @@
+// <!-- Change the text of a heading when a button is clicked. -->
+
+const heading = document.queryselector("h1");
+const headingBtn = document.querySelector("#headingBtn");
+
+headingBtn.addEventListener("click", () => {
+heading.textContent = "Heading Changed!";
+});
+
+
+   
+ 
+
